3419d0c3ec94c93570b04cc1bc4431777896b5ff
Lucas Almeida Petrolli <lucaslap27@gmail.com>
2025-03-04 13:58:56 -0400

    feat: ajuste de cors e no html para gerar o curriculo

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:3000", "http://localhost:3000"])
+    # CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
     from app.routes import main_bp  # Importa o Blueprint das rotas
     app.register_blueprint(main_bp)  # Registra o Blueprint no app
--- backend/services/gerar_curriculo.py
+++ backend/services/gerar_curriculo.py
@@ -1,8 +1,33 @@
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 
+
+def formatar_data_pt_br(data_str):
+    try:
+        data_obj = datetime.strptime(data_str, "%Y-%m-%d")
+        return data_obj.strftime("%d/%m/%Y")
+    except (ValueError, TypeError):
+        return data_str
+
+def formatar_telefone_br(telefone_str):
+    if not telefone_str:
+        return telefone_str  # Retorna string vazia ou None sem formatar
+
+    # Remove caracteres não numéricos
+    numeros = ''.join(filter(str.isdigit, telefone_str))
+    tamanho = len(numeros)
+
+    if tamanho == 10:  # Formato com DDD e 8 dígitos (ex: 11 1234-5678)
+        return f"({numeros[:2]}) {numeros[2:6]}-{numeros[6:]}"
+    elif tamanho == 11: # Formato com DDD e 9 dígitos (ex: 11 91234-5678)
+        return f"({numeros[:2]}) {numeros[2:7]}-{numeros[7:]}"
+    else:
+        return telefone_str
 def gerar_curriculo(dados):
     env = Environment(loader=FileSystemLoader("templates"))
+    env.filters['formatar_data_pt_br'] = formatar_data_pt_br
+    env.filters['formatar_telefone_br'] = formatar_telefone_br
     template = env.get_template("curriculo.html")
     html = template.render(dados=dados)
     caminho_wkhtmltopdf = "/home/lucas/.asdf/shims/wkhtmltopdf"  # Atualize com o resultado do "which wkhtmltopdf"
